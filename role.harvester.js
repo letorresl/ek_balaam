4f117683e2bef3342b0a888310cd97fef99ad7bb
Luis Torres <luis.e.torreslopez@gmail.com>
2018-05-30 10:56:25 -0500

    Homogeneizacion con demas roles

--- role.harvester.js
+++ role.harvester.js
@@ -2,13 +2,17 @@ var roleHarvester = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-        if(creep.carry.energy < creep.carryCapacity) {
-            var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
-            }
+        
+        if(creep.memory.storing && creep.carry.energy == 0) {
+            creep.memory.storing = false;
+            creep.say('Recolectando');
         }
-        else {
+        if(!creep.memory.storing && creep.carry.energy == creep.carryCapacity) {
+            creep.memory.storing = true;
+            creep.say('Almacenando');
+        }
+        
+        if(creep.memory.storing) {
             var targets = creep.room.find(FIND_STRUCTURES, {
                 filter: (structure) => {
                     return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
@@ -19,6 +23,12 @@ var roleHarvester = {
                 if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
                 }
+            }
+        }
+        else {
+            var sources = creep.room.find(FIND_SOURCES);
+            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
             }
         }
     }
