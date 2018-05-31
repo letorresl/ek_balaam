b59beffe6a36d30e54df0ab3038e26c16fef1f9b
Luis Torres <luis.e.torreslopez@gmail.com>
2018-05-31 01:34:04 -0500

    Reparacion de estructuras

--- modelo_estandar/role.builder.js
+++ modelo_estandar/role.builder.js
@@ -13,6 +13,14 @@ var roleBuilder = {
         }
 
         if(creep.memory.building) {
+            /** Reparacion de estructuras dañadas **/
+	        var closestDamagedStructure = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+	            filter: (structure) => structure.hits < structure.hitsMax && structure.hits < 1000
+	        });
+	        if(closestDamagedStructure) {
+	            creep.repair(closestDamagedStructure);
+	        }
+
             var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
             if(targets.length) {
                 if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
