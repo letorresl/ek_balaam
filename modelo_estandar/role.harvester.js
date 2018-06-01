695d3581077b50dc0e53387a80e437fa2fd84499
Luis Torres <luis.e.torreslopez@gmail.com>
2018-06-01 08:33:51 -0500

    la propiedad store solo aplica para el contenedor

--- modelo_estandar/role.harvester.js
+++ modelo_estandar/role.harvester.js
@@ -21,13 +21,16 @@ var roleHarvester = {
             var targets = creep.room.find(FIND_STRUCTURES, {
                 filter: (structure) => {
                     return (
-                        structure.structureType == STRUCTURE_EXTENSION ||
-                        structure.structureType == STRUCTURE_SPAWN ||
-                        structure.structureType == STRUCTURE_TOWER ||
-                        structure.structureType == STRUCTURE_CONTAINER
-                    ) && (
-                        structure.energy < structure.energyCapacity ||
-                        structure.store[RESOURCE_ENERGY] < structure.storeCapacity
+                        (
+                            structure.structureType == STRUCTURE_EXTENSION ||
+                            structure.structureType == STRUCTURE_SPAWN ||
+                            structure.structureType == STRUCTURE_TOWER
+                        ) && (
+                            structure.energy < structure.energyCapacity
+                        ) || (
+                            structure.structureType == STRUCTURE_CONTAINER &&
+                            structure.store[RESOURCE_ENERGY] < structure.storeCapacity   
+                        )
                     );
                 }
             });
