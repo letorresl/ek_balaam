a5d9b76b873a2751d35645c8a5d3f814a9c15073
Luis Torres <luis.e.torreslopez@gmail.com>
2018-06-03 13:10:58 -0500

    actualizacion towerAI

--- modelo_estandar/tower.ai.js
+++ modelo_estandar/tower.ai.js
@@ -3,17 +3,18 @@ var towerAI = {
     /** @param {Tower} tower **/
     run: function(tower) {
         
-	    var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
-	        filter: (structure) => structure.hits < 2000
-	    });
-	    if(closestDamagedStructure) {
-	        tower.repair(closestDamagedStructure);
-	    }
-
 	    var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
 	    if(closestHostile) {
 	        tower.attack(closestHostile);
 	    }
+        else {
+	        var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+	            filter: (structure) => structure.hits < 2000
+	        });
+	        if(closestDamagedStructure) {
+	            tower.repair(closestDamagedStructure);
+	        }
+        }
     }
 };
 
